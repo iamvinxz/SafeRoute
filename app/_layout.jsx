c2b2d6a1c75213ee35c13fa548e15d770811498d
John Khervin <john.khervin13@gmail.com>
2026-05-04 15:50:04 +0800

    feat: added safe area provider, and titile maps

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,11 +4,12 @@ import { useFonts } from "expo-font";
 import { Tabs } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const _layout = () => {
   const [fontsLoaded, fontError] = useFonts({
-    "Montserrat": require("../assets/fonts/Montserrat-Medium.ttf"),
-    "Montserrat-Bold": require("../assets/fonts/Montserrat-Bold.ttf")
+    Montserrat: require("../assets/fonts/Montserrat-Medium.ttf"),
+    "Montserrat-Bold": require("../assets/fonts/Montserrat-Bold.ttf"),
   });
 
   React.useEffect(() => {
@@ -21,17 +22,32 @@ const _layout = () => {
     return null;
   }
   return (
-    <Tabs tabBar={(props) => <TabBar {...props} />}>
-      <Tabs.Screen
-        name="index"
-        options={{ title: "Home", headerShown: false }}
-      />
-      <Tabs.Screen name="maps" options={{ title: "", headerShown: false }} />
-      <Tabs.Screen
-        name="menu"
-        options={{ title: "Menu", headerShown: false }}
-      />
-    </Tabs>
+    <SafeAreaProvider>
+      <Tabs
+        tabBar={(props) => <TabBar {...props} />}
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: "transparent",
+            borderTopWidth: 0,
+            elevation: 0,
+            position: "absolute",
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{ title: "Home", headerShown: false }}
+        />
+        <Tabs.Screen
+          name="maps"
+          options={{ title: "Maps", headerShown: false }}
+        />
+        <Tabs.Screen
+          name="menu"
+          options={{ title: "Menu", headerShown: false }}
+        />
+      </Tabs>
+    </SafeAreaProvider>
   );
 };
 
