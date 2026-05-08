11e6e4a8aa5ecbd4a978791a61dfa722c6eff56e
John Khervin <john.khervin13@gmail.com>
2026-05-08 08:57:42 +0800

    feat: svg drawer for tab bar

--- components/tabBarPath.js
+++ components/tabBarPath.js
@@ -6,10 +6,15 @@ export const TAB_BAR_HEIGHT = 65;
 export const HUMP_HEIGHT = 20;
 
 export const getHumpPath = (selectedIndex, tabCount, totalHeight) => {
+  // Guard against invalid inputs
+  if (!tabCount || tabCount <= 0 || selectedIndex < 0) {
+    return `M0,${HUMP_HEIGHT}L${SCREEN_WIDTH},${HUMP_HEIGHT}L${SCREEN_WIDTH},${totalHeight ?? TAB_BAR_HEIGHT}L0,${totalHeight ?? TAB_BAR_HEIGHT}Z`;
+  }
+
   const tabW = SCREEN_WIDTH / tabCount;
   const cx = tabW * selectedIndex + tabW / 2;
   const hw = 30;
-  const H = totalHeight;
+  const H = totalHeight ?? TAB_BAR_HEIGHT;
   const W = SCREEN_WIDTH;
 
   return `
