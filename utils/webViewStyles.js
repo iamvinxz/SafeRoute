2220f13150028ed5555666a601f074181d753c88
iamvinxz <john.khervin13@gmail.com>
2026-05-08 14:25:05 +0800

    feat: style for pin locations popup

---
+++ utils/webViewStyles.js
@@ -0,0 +1,25 @@
+export const webViewStyles = {
+  popup: {
+    container: `
+      min-width: 60px'
+      max-width: 180px;
+    `,
+    description: `
+      font-size: 10px;
+      color: #636363;
+      line-height: 2.3;
+      word-break: break-word;
+      display: block;
+    `,
+    date: `
+      font-size: 8px;
+      color: #797878;
+      display: block;
+    `,
+    column: `
+      display: flex;
+      flex-direction: column;
+      gap: 2px;
+    `,
+  },
+};
