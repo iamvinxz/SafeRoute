cf204d7832c498696cbcdb3efe987ed85d8a7a4f
John Khervin <john.khervin13@gmail.com>
2026-05-08 12:38:09 +0800

    feat: endpoint path for all pin locations

--- redux/Endpoint.js
+++ redux/Endpoint.js
@@ -4,3 +4,4 @@ export const GET_TINAJEROS =
   "/2023/geojson/municities/hires/bgysubmuns-municity-1380400000.0.1.json";
 export const GET_SEGMENTS = "/api/mark/segment/all";
 export const LOGIN = "/api/auth/login";
+export const GET_ALL_PINNED_LOCATIONS = "/api/mark/pin/all";
