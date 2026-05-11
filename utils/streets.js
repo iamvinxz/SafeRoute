f92da3d48e7a33457f500a684506deeeb054f8df
iamvinxz <john.khervin13@gmail.com>
2026-05-11 17:13:47 +0800

    feat: list of streets for dropdown menu

---
+++ utils/streets.js
@@ -0,0 +1,137 @@
+export const streets = [
+  {
+    name: "Goldendale Ave",
+  },
+  {
+    name: "Saint James St.",
+  },
+  {
+    name: "Sisa Ext.",
+  },
+  {
+    name: "Platinum St.",
+  },
+  {
+    name: "Plutonium St.",
+  },
+  {
+    name: "Arasity St.",
+  },
+  {
+    name: "Doña Victoria St.",
+  },
+  {
+    name: "Magnesium St.",
+  },
+  {
+    name: "Iron St.",
+  },
+  {
+    name: "Cobalt St.",
+  },
+  {
+    name: "Titanium St.",
+  },
+  {
+    name: "Zinc St.",
+  },
+  {
+    name: "Tungsten St.",
+  },
+  {
+    name: "Silver St.",
+  },
+  {
+    name: "Copper St.",
+  },
+  {
+    name: "Mercury St.",
+  },
+  {
+    name: "Nickel St.",
+  },
+  {
+    name: "Celia St.",
+  },
+  {
+    name: "Governor Wenceslao Pascual Avenue",
+  },
+  {
+    name: "B. Rivera St.",
+  },
+  {
+    name: "Crispin St.",
+  },
+  {
+    name: "D. Dela Cruz St.",
+  },
+  {
+    name: "A. Dizon St.",
+  },
+  {
+    name: "Sarmiento St.",
+  },
+  {
+    name: "Sanchez St.",
+  },
+  {
+    name: "Colonel Espiritu St.",
+  },
+  {
+    name: "Colonel Espiritu Ext.",
+  },
+  {
+    name: "Kab Martin St.",
+  },
+  {
+    name: "Mesina St.",
+  },
+  {
+    name: "Sevilla St.",
+  },
+  {
+    name: "Talilong St.",
+  },
+  {
+    name: "Kaybasco St.",
+  },
+  {
+    name: "Villarba St.",
+  },
+  {
+    name: "Bustamante St.",
+  },
+  {
+    name: "M.H Del Pilar St.",
+  },
+  {
+    name: "Sanciangco St.",
+  },
+  {
+    name: "Mañalac St.",
+  },
+  {
+    name: "Sociego St.",
+  },
+  {
+    name: "Trabajo St.",
+  },
+  {
+    name: "Ezperanza St.",
+  },
+  {
+    name: "Actividad St.",
+  },
+  {
+    name: "Concha St.",
+  },
+  {
+    name: "Mejorada St.",
+  },
+  {
+    name: "Perla St.",
+  },
+  {
+    name: "Kaingin Road St.",
+  },
+];
