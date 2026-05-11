ac87de4524be2a2ca6cdb1e85dd2d67b66691f4f
John Khervin <john.khervin13@gmail.com>
2026-05-11 12:33:59 +0800

    feat: set modal for indexing tabs layout

--- app/screens/_layout.jsx
+++ app/screens/_layout.jsx
@@ -1,10 +1,12 @@
+import CreateFloodReport from "@/components/CreateFloodReport";
 import TabBar from "@/components/TabBar";
 import { Redirect, Tabs } from "expo-router";
+import { StyleSheet, View } from "react-native";
 import { useSelector } from "react-redux";
 
 const ScreensLayout = () => {
   const { isAuthenticated, token } = useSelector((state) => state.auth);
-
+  const { isOpen } = useSelector((state) => state.modal);
   console.log("isAuthenticated:", isAuthenticated, "token:", token);
 
   if (!isAuthenticated || !token) {
@@ -12,31 +14,41 @@ const ScreensLayout = () => {
   }
 
   return (
-    <Tabs
-      tabBar={(props) => <TabBar {...props} />}
-      screenOptions={{
-        tabBarStyle: {
-          backgroundColor: "transparent",
-          borderTopWidth: 0,
-          elevation: 0,
-          position: "absolute",
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="home/index"
-        options={{ title: "Home", headerShown: false }}
-      />
-      <Tabs.Screen
-        name="maps/maps"
-        options={{ title: "Maps", headerShown: false }}
-      />
-      <Tabs.Screen
-        name="menu/menu"
-        options={{ title: "Menu", headerShown: false }}
-      />
-    </Tabs>
+    <View style={style.container}>
+      <Tabs
+        tabBar={(props) => <TabBar {...props} />}
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: "transparent",
+            borderTopWidth: 0,
+            elevation: 0,
+            position: "absolute",
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="home/index"
+          options={{ title: "Home", headerShown: false }}
+        />
+        <Tabs.Screen
+          name="maps/maps"
+          options={{ title: "Maps", headerShown: false }}
+        />
+        <Tabs.Screen
+          name="menu/menu"
+          options={{ title: "Menu", headerShown: false }}
+        />
+      </Tabs>
+
+      {isOpen && <CreateFloodReport />}
+    </View>
   );
 };
 
 export default ScreensLayout;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
