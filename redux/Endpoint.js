7c7cb304ba9313a00793695dcbf80a22e81f6472
John Khervin <john.khervin13@gmail.com>
2026-05-12 20:04:19 +0800

    feat: endpoint path for register

--- redux/Endpoint.js
+++ redux/Endpoint.js
@@ -4,5 +4,6 @@ export const GET_TINAJEROS =
   "/2023/geojson/municities/hires/bgysubmuns-municity-1380400000.0.1.json";
 export const GET_SEGMENTS = "/api/mark/segment/all";
 export const LOGIN = "/api/auth/login";
+export const REGISTER = "/api/auth/register";
 export const GET_ALL_PINNED_LOCATIONS = "/api/mark/pin/all";
 export const CREATE_FLOOD_REPORT = "/api/floods/report";
