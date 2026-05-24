fff9b2cce4ceef7541a8e4a7b609ae8aaff03c75
John Khervin <john.khervin13@gmail.com>
2026-05-24 15:25:50 +0800

    feat: added notification channel

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,11 +2,23 @@ import "@/global.css";
 import { store } from "@/store.js";
 import messaging from "@react-native-firebase/messaging";
 import { useFonts } from "expo-font";
+import * as Notification from "expo-notifications";
 import { Slot } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import React from "react";
+import { Platform } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
+
+//notification channel
+if (Platform.OS === "android") {
+  Notification.setNotificationChannelAsync("flood_alerts", {
+    name: "Flood Alerts",
+    importance: Notification.AndroidImportance.HIGH,
+    sound: "default",
+    vibrationPattern: [0, 250, 250, 250],
+  });
+}
 
 // ✅ background handler
 messaging().setBackgroundMessageHandler(async (remoteMessage) => {
