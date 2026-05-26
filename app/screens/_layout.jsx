da4e85479aa747db47bfd670203d7bcdfdfa3024
John Khervin <john.khervin13@gmail.com>
2026-05-27 00:07:29 +0800

    feat: added restore sos state to handle sos status when refreshed

--- app/screens/_layout.jsx
+++ app/screens/_layout.jsx
@@ -1,15 +1,52 @@
 import CreateFloodReport from "@/components/CreateFloodReport";
 import TabBar from "@/components/TabBar";
+import { api } from "@/redux/APIService";
+import { setSosAlert } from "@/states/sosAlertSlice";
 import { useFCM } from "@/utils/useFCM";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Redirect, Tabs } from "expo-router";
+import { useEffect } from "react";
 import { StyleSheet, View } from "react-native";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const ScreensLayout = () => {
+  const dispatch = useDispatch();
   const { isAuthenticated, token } = useSelector((state) => state.auth);
   const { isOpen } = useSelector((state) => state.modal);
 
   useFCM();
+
+  useEffect(() => {
+    const restoreSosState = async () => {
+      try {
+        if (!token) return;
+        const stored = await AsyncStorage.getItem("activeSos");
+
+        if (!stored) return;
+
+        const { sosId } = JSON.parse(stored);
+
+        const data = await dispatch(
+          api.endpoints.getSosById.initiate(sosId),
+        ).unwrap();
+
+        if (data.sos.status !== "resolved" && data.sos.status !== "cancelled") {
+          dispatch(setSosAlert({ sosId, status: data.sos.status }));
+          await AsyncStorage.setItem(
+            "activeSos",
+            JSON.stringify({ sosId, status: data.sos.status }),
+          );
+        } else {
+          await AsyncStorage.removeItem("activeSos");
+        }
+      } catch (e) {
+        console.error("Failed to restore SOS state:", e);
+        await AsyncStorage.removeItem("activeSos");
+      }
+    };
+
+    restoreSosState();
+  }, [token]);
 
   if (!isAuthenticated || !token) {
     //todo: validate the token expiry
